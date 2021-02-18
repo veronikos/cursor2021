5bf8eb8157b09ea788853897d332e7822ecb78ed
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-02-18 14:50:48 +0200

    Add delete Tab before hyphenation in console log

---
+++ Notes/lecture2.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+const day = prompt("what is the day today?");
+
+if (day === "friday") {
+	alert("you are right");
+	// const bestOption = prompt("What do you prefer to do on Friday?");
+	// if (bestOption === "bar") alert('yes, it is a good idea');
+} else alert(`${day} is not a correct answer`);
+
