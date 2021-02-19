5e1ab227a310b3c65768636258baacba53de84c8
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-02-19 18:40:26 +0200

    2 homework

---
+++ 02-hw-conditions/main.js
@@ -0,0 +1,39 @@
+let numberN = prompt("Enter the first number");
+
+if (parseInt(numberN) !== NaN) {
+    const numberM = prompt(`First number is: ${parseInt(numberN)}. Enter the second number`);
+        if ((parseInt(numberM) !== NaN) && (numberN < numberM)) {
+
+            const countEvens = prompt(`Do you want to include even numbers? (enter: true, false)`);
+            alert(typeof countEvens);
+            if  (countEvens === "true") {
+                let x = parseInt(numberN);
+        
+                 while (numberN < numberM) {
+                    numberN++;
+                    x += numberN;
+                } alert(`Sum is ${x}!`);
+            }  else {
+                let x = parseInt(numberN);
+        
+                 while (numberN < numberM) {
+                    numberN++;
+                    if (num % 2) x += numberN;
+                } alert(`Sum is ${x}!`);
+            }   
+        }   else {
+        alert(`Second number (${numberM}) must be bigger that first (${numberN}). Play again`)
+    }
+}
+
+// часть для того где не считать парные числа
+
+// let numberN = 30;
+// let numberM = 40;
+// let x = parseInt(numberN);
+        
+//  while (numberN < numberM) {
+//  	numberN++;
+//   x += numberN;
+//   if(numberN%2) x -= numberN;
+// 	} console.log(`Sum is ${x}!`);
