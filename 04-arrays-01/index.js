a0d821ebf7882af22cfe448e656b2372ef1f9fd3
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-02-28 22:32:07 +0200

    Done task HW#4. Arrays.Part1

--- 04-arrays-01/index.js
+++ 04-arrays-01/index.js
@@ -1,1 +1,84 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const themes = [
+  "Диференційне рівняння",
+  "Теорія автоматів",
+  "Алгоритми і структури даних",
+];
+const marks = [4, 5, 5, 3, 4, 5];
 
+// divide into groups girl+boy
+function girlsBoysPairs(arr) {
+  let studentsArray = [...arr];
+  let girls = [];
+  let boys = [];
+  let pairs = [];
+  for (let i = 0; i < studentsArray.length; i++) {
+    if (studentsArray[i].endsWith("а")) {
+      girls.push(studentsArray[i]);
+    } else {
+      boys.push(studentsArray[i]);
+    }
+  }
+
+  for (let i = 0; i < girls.length; i++) {
+    pairs.push([boys[i], girls[i]]);
+  }
+  return pairs;
+}
+
+const gbPairs = girlsBoysPairs(students);
+
+console.log(gbPairs);
+
+//  combine pairs and themes
+function studentsThemes(studentsArray, themesRandom) {
+  let themesArray = [...themesRandom];
+  let pairsStudents = girlsBoysPairs(studentsArray);
+  let themesAssignment = [];
+
+  for (let i = 0; i < themesArray.length; i++) {
+    themesAssignment.push([pairsStudents[i].join(" i "), themesArray[i]]);
+  }
+  return themesAssignment;
+}
+
+const pairsThemesCombined = studentsThemes(students, themes);
+console.log(pairsThemesCombined);
+
+// Assignment of marks to students
+function studentMarks(studentsArray, marksArray) {
+  let studentsWaiting = [...studentsArray];
+  let marksGiven = [...marksArray];
+  let marksAssigned = [];
+  for (let i = 0; i < marksGiven.length; i++) {
+    marksAssigned.push([studentsWaiting[i], marksGiven[i]]);
+  }
+  return marksAssigned;
+}
+
+const marksToStudents = studentMarks(students, marks);
+console.log(marksToStudents);
+
+// Rating of student pairs projects
+function getRandomMark(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
+
+//  у меня в функции исходный массив не мутирует,
+// даже без использование ...spread. тут нету ошибки?
+
+function ratingofPairs(studentsArray, themesArray) {
+  let studentsWaiting = [...studentsArray];
+  let themesGiven = [...themesArray];
+  let themesAssigned = [];
+  for (let i = 0; i < themesGiven.length; i++) {
+    themesAssigned.push([
+      studentsWaiting[i].join(" i "),
+      themesGiven[i],
+      getRandomMark(1, 5),
+    ]);
+  }
+  return themesAssigned;
+}
+const pairsRated = ratingofPairs(gbPairs, themes);
+console.log(pairsRated);
