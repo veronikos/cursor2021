5ca60d53f8bc538f6df6187e3b2977947daae8f0
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-12 17:14:05 +0200

    mentor changes to hw5

--- 05-arrays-02/main.js
+++ 05-arrays-02/main.js
@@ -1,116 +1,82 @@
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 // ex. 1
 function getRandomArray(length, min, max) {
   const randomArray = [];
-  function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
 
   for (let i = 0; i < length; i++) {
     randomArray.push(getRandomNumber(min, max));
   }
+
   return randomArray;
 }
 
 console.log(` ex.1:`, getRandomArray(9, 1, 25));
 
 // ex. 3
-function getAverage(numbers) {
-  const myNumbers = [...numbers];
-  let myIntegerNumbers = [];
+function getAverage(...numbers) {
+  const myIntegerNumbers = numbers.filter((item) => Number.isInteger(item));
 
-  function isIntegerCallback(number) {
-    if (!Number.isInteger(number)) {
-      return false;
-    }
-    return true;
-  }
-
-  myIntegerNumbers = myNumbers.filter(isIntegerCallback);
-
-  const reducerCallback = (accumulator, currentValue) =>
-    accumulator + currentValue;
-  myIntegerNumbers.reduce(reducerCallback);
-
-  return myIntegerNumbers.reduce(reducerCallback) / myIntegerNumbers.length;
+  return (
+    myIntegerNumbers.reduce((acc, item) => acc + item) / myIntegerNumbers.length
+  );
 }
 
+// так? со скобками?
 console.log(
   ` ex.3: getAverage(15, 25.2, 35, 78.4, 5, 6, 10): `,
-  getAverage([15, 25.2, 35, 78.4, 5, 6, 10])
+  getAverage(15, 25.2, 35, 78.4, 5, 6, 10)
 );
 
 // ex. 4
 function getMedian(...numbers) {
-  const myNumbers = [...numbers];
-  let myIntegerNumbers = [];
+  const myIntegerNumbers = numbers.filter((item) => Number.isInteger(item));
+  const sortedNumbers = myIntegerNumbers.sort((a, b) => a - b);
   let countMedian = null;
 
-  for (let i = 0; i < myNumbers.length; i++) {
-    if (!Number.isInteger(myNumbers[i])) {
-      continue;
-    }
-    myIntegerNumbers.push(myNumbers[i]);
-  }
-
-  let sortedNumbers = myIntegerNumbers.sort((a, b) => a - b);
+  // не понимаю как это через фильтр реализовать...
   if (sortedNumbers.length % 2 === 1) {
-    let medianIndex = Number((sortedNumbers.length / 2).toFixed()) - 1;
+    const medianIndex = Number((sortedNumbers.length / 2).toFixed()) - 1;
     countMedian = sortedNumbers[medianIndex];
   } else {
-    let secondMedianIndex = Number((sortedNumbers.length / 2).toFixed());
-    let firstMedianIndex = secondMedianIndex - 1;
+    const secondMedianIndex = Number((sortedNumbers.length / 2).toFixed());
+    const firstMedianIndex = secondMedianIndex - 1;
+
     countMedian =
       (sortedNumbers[secondMedianIndex] + sortedNumbers[firstMedianIndex]) / 2;
   }
+
   return countMedian;
 }
 console.log(`ex.4:`, getMedian(3, 0.5, 5, 6.7));
 
 // ex.5
-function filterEvenNumbers(...numbers) {
-  const numbersArray = [...numbers];
-  let evenNumbers = numbersArray.filter(function (number) {
-    return number % 2 !== 0;
-  });
-  return evenNumbers;
-}
-console.log(` ex.5:`, filterEvenNumbers(2, 187, -0.9, 3789, -5));
+const filterEvenNumbers = (...numbers) => numbers.filter((el) => el % 2 !== 0);
+
+console.log(` ex.5:`, filterEvenNumbers(2, 187, -0.9, 3788, -5));
 
 // ex.6
 function countPositiveNumbers(...numbers) {
-  const numbersArray = [...numbers];
-  let positiveNumbers = numbersArray.filter(function (number) {
-    return number > 0;
-  });
+  const positiveNumbers = numbers.filter((num) => num > 0);
+
   return positiveNumbers.length;
 }
+
 console.log(`ex.6:`, countPositiveNumbers(-3, 5, 7, 100, -6, -5, 6));
 
 // ex. 7
-function getDividedByFive(...numbers) {
-  const numbersArray = [...numbers];
-  let dividedByFive = numbersArray.filter(function (number) {
-    return number % 5 === 0;
-  });
-  return dividedByFive;
-}
+const getDividedByFive = (...numbers) => numbers.filter((num) => num % 5 === 0);
+
 console.log(
   ` ex.7:`,
   getDividedByFive(-3, 5, 45, 500, 5915, 7, 100, -6, -5, 6)
 );
 
 // Ex. 8
-function replaceBadWords(comment) {
-  const badWords = ["fuck", "shit"];
-  let clearPhrase = comment;
-  for (let i = 0; i < badWords.length; i++) {
-    clearPhrase = clearPhrase.replace(badWords[i], "****");
-  }
-  return clearPhrase;
-}
-// can't figure out how to insert not fixed amount of "*"
-// for example = "bad".length * "*" => "***"
-// "fuck".length * "*" => "****"
+const replaceBadWords = (comment) =>
+  comment.replaceAll("fuck", "****").replaceAll("shit", "****");
 
 console.log(
   `ex.8:`,
@@ -121,12 +87,15 @@ console.log(
 function divideByThree(enterString) {
   const startString = enterString.split(" ").join("");
   let finishString = [];
+
   for (let i = 0; i < startString.length; i += 3) {
     finishString.push(startString.substring(i, i + 3));
   }
+
   return finishString;
 }
-console.log(` ex.9:`, divideByThree("LOVE me tender"));
+
+console.log(` ex.9:`, divideByThree("LOVE"));
 
 //!!!!!! ex. 10: Generate unique reshuffle of letters
 // function generateCombinations(word) {
