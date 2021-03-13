4a95f096bd5f3a2de23a25fa00734e89e474a1c3
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-13 11:46:57 +0200

    format doc

--- 07-this-context/main.js
+++ 07-this-context/main.js
@@ -1,47 +1,50 @@
 function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
+  return Math.round(Math.random() * (max - min) + min);
+}
 
 const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const lithuania = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
 function getMyTaxes(salary) {
-    return this.tax * salary
+  return this.tax * salary;
 }
 
 console.log(`
 Tax from 100 in Ukraine: ${getMyTaxes.call(ukraine, 100)}
 Tax from 100 in Latvia: ${getMyTaxes.call(latvia, 100)}
 Tax from 100 in Lithuania: ${getMyTaxes.call(lithuania, 100)}
-`)
+`);
 
 function getMiddleTaxes() {
-    return this.tax * this.middleSalary
+  return this.tax * this.middleSalary;
 }
 
-console.log(` Middle IT-tax rate in Ukraine:`, getMiddleTaxes.call(ukraine))
-console.log(` Middle IT-tax rate in Latvia:`, getMiddleTaxes.call(latvia))
-console.log(` Middle IT-tax rate in Lithuania:`, getMiddleTaxes.call(lithuania))
+console.log(` Middle IT-tax rate in Ukraine:`, getMiddleTaxes.call(ukraine));
+console.log(` Middle IT-tax rate in Latvia:`, getMiddleTaxes.call(latvia));
+console.log(
+  ` Middle IT-tax rate in Lithuania:`,
+  getMiddleTaxes.call(lithuania)
+);
 
 function getTotalTaxes() {
-    return this.tax * this.middleSalary * this.vacancies
+  return this.tax * this.middleSalary * this.vacancies;
 }
 
-console.log(`Total IT-tax rate in Ukraine:`, getTotalTaxes.call(ukraine))
-console.log(`Total IT-tax rate in Latvia:`, getTotalTaxes.call(latvia))
-console.log(`Total IT-tax rate in Lithuania:`, getTotalTaxes.call(lithuania))
+console.log(`Total IT-tax rate in Ukraine:`, getTotalTaxes.call(ukraine));
+console.log(`Total IT-tax rate in Latvia:`, getTotalTaxes.call(latvia));
+console.log(`Total IT-tax rate in Lithuania:`, getTotalTaxes.call(lithuania));
 
-function getMySalary() {
-    const salary = getRandomNumber(1500, 2000);
-    const taxInCountry = this.tax * salary
-    const object = {
-        salary: salary,
-        taxes: taxInCountry,
-        profit: salary - taxInCountry
-    }
+function getMySalary(country) {
+  const salary = getRandomNumber(1500, 2000);
+  const taxes = getMyTaxes.call(country, salary);
+  const object = {
+    salary,
+    taxes,
+    profit: salary - taxes,
+  };
 
-    return object
+  return object;
 }
 
-setInterval(`console.log(getMySalary.call(ukraine))`, 10000)
+setInterval(`console.log(getMySalary(ukraine))`, 10000);
