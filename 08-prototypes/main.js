989f6f70692dd7cf235286cbf80c08439e8c7403
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-14 17:45:32 +0200

    do hw-8

--- 08-prototypes/main.js
+++ 08-prototypes/main.js
@@ -1,61 +1,92 @@
 class Student {
-  constructor(university, course, fullName) {
+  constructor(university, course, fullName, startMarks) {
     this.university = university;
     this.course = course;
     this.fullName = fullName;
-    this.startMarks = [5, 4, 4, 5];
+    this.startMarks = startMarks;
+  }
+
+  dismiss() {
+    this.startMarks = null;
+  }
+
+  recover() {
+    this.startMarks = [];
+  }
+
+  getAveragemark() {
+    return getAverage(...this.startMarks);
   }
 
   get getInfo() {
-    return `
-    Студентка ${this.course}го курсу ${this.university}, ${this.fullName}
-    `;
+    return `Студентка ${this.course}го курсу ${this.university}, ${this.fullName}`;
   }
 
-  get getMarks() {
-    return this.startMarks
+  get marks() {
+    return this.startMarks;
   }
 
   set setMarks(newMark) {
-      this.startMarks.push(newMark)
+    if (this.startMarks !== null) this.startMarks.push(newMark);
   }
-
-  get getAveragemark() {
-    return getAverage(...this.startMarks) 
-  }
-
 }
 
-const veronika = new Student("Вищої Школи Економіки", 4, "Вероніка Костенко")
+const hermione = new Student(
+  "Гоґвортської школи чарів і чаклунства",
+  4,
+  "Герміона Грейнджер",
+  [5, 4, 4, 5]
+);
 
-console.log (veronika.getInfo)
-console.log(veronika.getMarks)
-veronika.setMarks = 5
-console.log(veronika.getMarks)
-console.log(veronika.getAveragemark)
-
+console.log(hermione.getInfo);
+console.log(hermione.marks);
+hermione.setMarks = 5;
+console.log(hermione.marks);
+console.log(hermione.getAveragemark());
+hermione.dismiss();
+console.log(hermione.marks);
+hermione.setMarks = 5;
+console.log(hermione.marks);
+hermione.recover();
+hermione.setMarks = 5;
+console.log(hermione.marks);
 
 /* Advanced */
 class BudgetStudent extends Student {
-    constructor (university, course, fullName) {
-        super(university, course, fullName)
+  constructor(university, course, fullName, startMarks) {
+    super(university, course, fullName, startMarks);
+    
+    this.getScholarship = function getScholarship() {
+      if (this.startMarks === null) {
+        console.log(`Ви не можете претендувати на стипендію`);
+      } else if (this.getAveragemark() >= 4) {
+        console.log(`Ви отримали 1400 грн. стипендії`);
+      } else {
+        console.log(
+          `Ваша середня оцінка ${this.getAveragemark()}, ви не можете отримати стипендію`
+        );
+      }
     }
 
-    get getScholarship() {
-        return `Ви отримали 1400 грн. стипендії`
-    }
+    setInterval(() => this.getScholarship(), 30000)
+  }
 }
 
-const monika = new BudgetStudent("Вищої Школи Економіки", 4, "Monika")
-console.log(monika.getScholarship)
-
-
+const monika = new BudgetStudent("Friends Series", 4, "Monika", [3, 4, 4, 5])
+console.log(monika.getInfo)
+console.log(monika.getAveragemark())
+monika.getScholarship()
+monika.dismiss()
+monika.getScholarship()
+monika.recover()
+monika.setMarks = 4
+monika.getScholarship()
 
 /* External functions */
 function getAverage(...numbers) {
-    const myIntegerNumbers = numbers.filter((item) => Number.isInteger(item));
-  
-    return (
-      myIntegerNumbers.reduce((acc, item) => acc + item) / myIntegerNumbers.length
-    );
-  }
+  const myIntegerNumbers = numbers.filter((item) => Number.isInteger(item));
+
+  return (
+    myIntegerNumbers.reduce((acc, item) => acc + item) / myIntegerNumbers.length
+  );
+}
