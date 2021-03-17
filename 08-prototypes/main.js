f244f89707b98a9f23f47f2cef31919d3493e2e6
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-17 13:29:59 +0200

    edit function dismiss/recover

--- 08-prototypes/main.js
+++ 08-prototypes/main.js
@@ -4,14 +4,15 @@ class Student {
     this.course = course;
     this.fullName = fullName;
     this.startMarks = startMarks;
+    this.activeStudent = true;
   }
 
   dismiss() {
-    this.startMarks = null;
+    this.activeStudent = false;
   }
 
   recover() {
-    this.startMarks = [];
+    this.activeStudent = true;
   }
 
   getAveragemark() {
@@ -19,37 +20,41 @@ class Student {
   }
 
   get getInfo() {
-    return `Студентка ${this.course}го курсу ${this.university}, ${this.fullName}`;
+    return `Student of ${this.course} year of study in ${this.university}, ${this.fullName}`;
   }
 
   get marks() {
-    return this.startMarks;
+    return this.activeStudent ? this.startMarks : null;
   }
 
   set setMarks(newMark) {
-    if (this.startMarks !== null) this.startMarks.push(newMark);
+    if (this.activeStudent) {
+      this.startMarks.push(newMark);
+    }
   }
 }
 
 const hermione = new Student(
-  "Гоґвортської школи чарів і чаклунства",
+  "Hogwarts School of Witchcraft and Wizardry",
   4,
-  "Герміона Грейнджер",
+  "Hermione Jean Granger",
   [5, 4, 4, 5]
 );
 
-console.log(hermione.getInfo);
-console.log(hermione.marks);
+console.log('Get info about a student:', hermione.getInfo)
+console.log('Get student marks:', hermione.marks)
 hermione.setMarks = 5;
-console.log(hermione.marks);
-console.log(hermione.getAveragemark());
+console.log('Get student marks after adding 5:', hermione.marks)
+console.log('Get average marks', hermione.getAveragemark())
 hermione.dismiss();
-console.log(hermione.marks);
-hermione.setMarks = 5;
-console.log(hermione.marks);
+console.log('Get marks after dismiss() of a student:', hermione.marks);
+hermione.setMarks = 3;
+console.log('Trying to set marks after dismiss() of a student:', hermione.marks);
 hermione.recover();
-hermione.setMarks = 5;
-console.log(hermione.marks);
+console.log('Get marks after recover() of a student:', hermione.marks);
+hermione.setMarks = 6;
+console.log('Set new mark "6" after recover() of a student:', hermione.marks);
+
 
 /* Advanced */
 class BudgetStudent extends Student {
@@ -57,14 +62,12 @@ class BudgetStudent extends Student {
     super(university, course, fullName, startMarks);
     
     this.getScholarship = function getScholarship() {
-      if (this.startMarks === null) {
-        console.log(`Ви не можете претендувати на стипендію`);
+      if (!this.activeStudent) {
+        return `Sorry, you cannot apply for a scholarship, you are not a student.`
       } else if (this.getAveragemark() >= 4) {
-        console.log(`Ви отримали 1400 грн. стипендії`);
+        return `You've got a scholarship of 1400 UAH.`
       } else {
-        console.log(
-          `Ваша середня оцінка ${this.getAveragemark()}, ви не можете отримати стипендію`
-        );
+        return `Your average score is ${this.getAveragemark()}, this is not enough to get a scholarship. Good luck next semester!`
       }
     }
 
@@ -72,15 +75,27 @@ class BudgetStudent extends Student {
   }
 }
 
-const monika = new BudgetStudent("Friends Series", 4, "Monika", [3, 4, 4, 5])
-console.log(monika.getInfo)
-console.log(monika.getAveragemark())
-monika.getScholarship()
+console.log("******")
+
+const monika = new BudgetStudent("L'académie de magie Beauxbâtons", 4, "Monika de L'Etoile", [4, 4, 4, 5])
+console.log('Get info about Budget student:', monika.getInfo)
+console.log('Get all Monika marks, just in case you are interested', monika.marks)
+console.log('Get average mark', monika.getAveragemark())
+console.log(`Can Monika get a scholarship?
+??
+${monika.getScholarship()}`)
 monika.dismiss()
-monika.getScholarship()
+console.log(`Can Monika get a scholarship after dismiss()?
+??
+${monika.getScholarship()}`)
 monika.recover()
-monika.setMarks = 4
-monika.getScholarship()
+console.log(`Can Monika get a scholarship after recover()? 
+??
+${monika.getScholarship()}`)
+monika.setMarks = 2
+console.log(`Can Monika get a scholarship after she get 2? 
+??
+${monika.getScholarship()}`)
 
 /* External functions */
 function getAverage(...numbers) {
