ec12f4b59edd359b2010e7f2a96abd9d894c0819
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-19 16:31:25 +0200

    add for cycle to generate sounds

--- 11-events/main.js
+++ 11-events/main.js
@@ -1,29 +1,39 @@
-const bassString = document.getElementById("bassString") 
+const bassString = document.getElementById("bassString")
+const cigaretteLighter = document.getElementById("cigaretteLighter")
+const openSigaretteLighter = document.getElementById("openSigaretteLighter")
+const croudLaugh = document.getElementById("croudLaugh")
+const mouseClick = document.getElementById("mouseClick")
+const horn = document.getElementById("horn")
+const rotarySwitch = document.getElementById("rotarySwitch")
+
+const soundsArray = [bassString, cigaretteLighter, openSigaretteLighter, croudLaugh, mouseClick, horn, rotarySwitch]
 
 function playSound(audio) {
-    audio.cloneNode()
-    audio.play();
-    console.log("Bass string played")
+    audio.cloneNode().play();
 }
 
 const btnA = document.getElementById("letterA")
+const btnS = document.getElementById("letterS")
+const btnD = document.getElementById("letterD")
+const btnF = document.getElementById("letterF")
+const btnG = document.getElementById("letterG")
+const btnH = document.getElementById("letterH")
 
-btnA.addEventListener("click", playSound.bind(null, bassString))
+const buttonsArray = [btnA, btnS, btnD, btnF, btnG, btnH];
 
-// var button = document.querySelector('.button');
+const listenersArray = [];
 
-// function setupSynth() {
-//   window.synth = new Tone.Synth({
-//     oscillator: {
-//       type: 'sine',
-//       modulationFrequency: 0.5
-//     },
-//     envelope: {
-//       attack: 0,
-//       decay: 0.2,
-//       sustain: 0,
-//       release: 0.5,
-//     }
-//   }).toMaster();
-// }
+for (let i = 0; i < buttonsArray.length; i++) {
+    buttonsArray[i].addEventListener('click', playSound.bind(null, soundsArray[i]));
+    const soundName = document.createElement('span')
+    let soundNameCase = (soundsArray[i].id).replace(/([A-Z][a-z])/g, " $1")
+    soundName.innerHTML = `${(soundNameCase[0].toUpperCase() + soundNameCase.slice(1).toLowerCase())}`
+    buttonsArray[i].appendChild(soundName)
+}
+
+function splitCamelCaseToString(s) {
+    return s.split(/(?=[A-Z])/).map(function(p) {
+        return p.charAt(0).toUpperCase() + p.slice(1);
+    }).join(' ');
+}
 
