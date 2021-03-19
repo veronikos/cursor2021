aa2a902ad600ee56bc45e4ca6958d195e982a8f2
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-19 13:27:05 +0200

    sound plays perfectly

--- 11-events/main.js
+++ 11-events/main.js
@@ -1,10 +1,29 @@
 const bassString = document.getElementById("bassString") 
 
-function playSound() {
-    bassString.play();
+function playSound(audio) {
+    audio.cloneNode()
+    audio.play();
     console.log("Bass string played")
 }
 
 const btnA = document.getElementById("letterA")
 
-btnA.addEventListener("click", playSound)
+btnA.addEventListener("click", playSound.bind(null, bassString))
+
+// var button = document.querySelector('.button');
+
+// function setupSynth() {
+//   window.synth = new Tone.Synth({
+//     oscillator: {
+//       type: 'sine',
+//       modulationFrequency: 0.5
+//     },
+//     envelope: {
+//       attack: 0,
+//       decay: 0.2,
+//       sustain: 0,
+//       release: 0.5,
+//     }
+//   }).toMaster();
+// }
+
