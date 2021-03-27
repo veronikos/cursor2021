7d9c977c2b4c59d705c250fb38c5ba1e00c3fe01
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-27 16:35:36 +0200

    think about Promise

--- 12-promise/main.js
+++ 12-promise/main.js
@@ -2,70 +2,71 @@
 // const currentTimestamp = String(Date.now())
 // console.log(String.fromCharCode(currentTimestamp.slice(-5)));
 
-let chineseString = '';
-function getRandomChinese(n){
+
+// let chineseString = '';
+// function getRandomChinese(n){
     
-    return new Promise((resolve) => {
-    let i = 0;
+//     return new Promise((resolve) => {
+//     let i = 0;
 
-    while (i < n) {
-        chineseString += String.fromCharCode(String(Date.now()).slice(-5));
-        i++
-    }
-    resolve('resolve')
-    })
+//     while (i < n) {
+//         chineseString += String.fromCharCode(String(Date.now()).slice(-5));
+//         i++
+//     }
+//     resolve('resolve')
+//     })
 
-}
+// }
 
-function vait50ms() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("timeout")
-    }, 50);
-    resolve()
-  })
-}
+// function vait50ms() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("timeout")
+//     }, 50);
+//     resolve()
+//   })
+// }
 
-console.log(getRandomChinese(1)
-.then(vait50ms)
-.then(() => getRandomChinese(1)))
+// console.log(getRandomChinese(1)
+// .then(vait50ms)
+// .then(() => getRandomChinese(1)))
 
-console.log(chineseString);
+// console.log(chineseString);
 
-// get date
+// // get date
 
-function getTimestamp() {
+// function getTimestamp() {
   
-  return new Promise ((resolve) => {
-    String(Date.now()).slice(-5)
-    resolve()
-  })
+//   return new Promise ((resolve) => {
+//     String(Date.now()).slice(-5)
+//     resolve()
+//   })
 
-}
+// }
 
-console.log(getTimestamp());
+// console.log(getTimestamp());
 
-getTimestamp().then(() => {
-  const string = getTimestamp();
-  return string
-})
+// getTimestamp().then(() => {
+//   const string = getTimestamp();
+//   return string
+// })
 
 
-const somethingSuccesfulHappened = true
+// const somethingSuccesfulHappened = true
 
-const myPromise =  new Promise((resolve, reject) => {
-  if(somethingSuccesfulHappened) {
-     const successObject = {
-        msg: 'Success',
-     }
-     resolve(successObject); 
-  } else {
-     const errorObject = {
-        msg: 'An error occured',
-     }
-     reject(errorObject);
-  }
-});
+// const myPromise =  new Promise((resolve, reject) => {
+//   if(somethingSuccesfulHappened) {
+//      const successObject = {
+//         msg: 'Success',
+//      }
+//      resolve(successObject); 
+//   } else {
+//      const errorObject = {
+//         msg: 'An error occured',
+//      }
+//      reject(errorObject);
+//   }
+// });
 
 
 // save to chineseString
@@ -127,29 +128,29 @@ const myPromise =  new Promise((resolve, reject) => {
 
 // WITH PROMISES REST-PUSHUPS
 
-function doPushups(n) {
+// function doPushups(n) {
 
-  return new Promise((resolve) => {
-    const time = n * 200;
+//   return new Promise((resolve) => {
+//     const time = n * 200;
 
-    setTimeout(() => {
-      console.log(n + " pushups done " + new Date().toLocaleString());
+//     setTimeout(() => {
+//       console.log(n + " pushups done " + new Date().toLocaleString());
 
-      resolve();
-    }, time);
+//       resolve();
+//     }, time);
 
-  });
-}
+//   });
+// }
 
-function rest() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("Rest is done " + new Date().toLocaleString());
+// function rest() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log("Rest is done " + new Date().toLocaleString());
 
-      resolve()
-    }, 1000);
-  });
-}
+//       resolve()
+//     }, 1000);
+//   });
+// }
 
 // async function cursorPushUps() {
 //   await doPushups(8);
@@ -158,3 +159,22 @@ function rest() {
 
 // cursorPushUps()
 
+function getRandomChinese() {
+
+  let random = ''
+  random = setInterval(() => getChineseSymbol(), 50)
+  return getChineseSymbol() + random
+
+}
+
+function getChineseSymbol() {
+
+  const date = String(Date.now()).slice(-5);
+  const symbol = String.fromCharCode(date)
+
+  return symbol
+}
+
+console.log(getChineseSymbol() + getChineseSymbol())
+
+console.log(getRandomChinese())
