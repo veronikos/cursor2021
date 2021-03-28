ee10b2976bf135a4c0587af535fdb5385d1dcfb2
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-28 23:17:08 +0300

    get required data from API

--- 13-server/main.js
+++ 13-server/main.js
@@ -1,34 +1,35 @@
-axios
-  .get("https://swapi.dev/api/people/")
-  .then((result) => {
-    const people = result.data.results;
-    const requiredInfo = people.map((p) => ({
-      name: p.name,
-      birth_Year: p.birth_year,
-      gender: p.gender,
-    }));
-    return requiredInfo;
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// axios
+//   .get("https://swapi.dev/api/films/2/")
+//   .then((res) => {
+//     return res.data;
+//   })
+//   .then(data => console.log(data))
 
-axios
-  .get("https://swapi.dev/api/films/2/")
-  .then((res) => {
-    return res.data.characters;
-  })
-  .then(
-    axios
-      .get("http://swapi.dev/api/people/1/")
-      .then((res) => res.data)
-      .then((res) => {
-        const character = {
-            Name: res.name,
-            Birth_year: res.birth_year,
-            Gender: res.gender
-        }
-        return character;
-    })
-    .then(data => console.log(data))
-  )
+async function getInfo() {
+    const filmsData = await axios.get("https://swapi.dev/api/films/2/");
+    const charactersUrls = filmsData.data.characters
+    const charactersShortInfo = {}
+
+    const body = document.querySelector("body");
+    const div = document.createElement('div')
+    
+    for (let i = 0; i < charactersUrls.length; i++) {
+      const data = await axios.get(`${charactersUrls[i]}`);
+      charactersShortInfo[i] = {
+        name: data.data.name,
+        birth_year: data.data.birth_year,
+        gender: data.data.gender,
+      };
+
+      div.innerHTML = `${i} :
+      Name: ${charactersShortInfo[i].name}
+      Birth year: ${charactersShortInfo[i].birth_year}
+      Gender: ${charactersShortInfo[i].gender}
+      `
+      body.append(div)
+    }
+
+    return charactersShortInfo
+}
+
+getInfo().then(data => console.log(data))
