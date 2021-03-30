5b7c9ba2ce60d96e9d523437e16513c5dd9d1f20
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-03-30 16:34:38 +0300

    add planets. work with next button

--- 13-server/main.js
+++ 13-server/main.js
@@ -1,9 +1,12 @@
 const getInfoButton = document.querySelector('.getInfo')
 getInfoButton.addEventListener('click', getInfo)
 
+const getPlanetsButton = document.querySelector('.getPlanets')
+getPlanetsButton.addEventListener('click', getPlanets)
+
 async function getInfo() {
-    const filmsData = await axios.get("https://swapi.dev/api/films/2/");
-    const charactersUrls = filmsData.data.characters
+  const filmsData = await axios.get("http://swapi.dev/api/films/2/");
+  const charactersUrls = filmsData.data.characters
     const charactersShortInfo = {}
 
     const field = document.querySelector('.field');
@@ -36,7 +39,7 @@ async function getInfo() {
         border-radius: 10px;
         width: 200px;
         height: 120px;
-        margin: 15px;        
+        margin: 15px;      
       `
       flex.appendChild(caracterCard)
       
@@ -70,4 +73,74 @@ async function getInfo() {
     return charactersShortInfo
 }
 
-// getInfo().then(data => console.log(data))
+async function getPlanets() {
+  const planetsData = await axios.get("https://swapi.dev/api/planets/");
+  const planetsObjects = planetsData.data.results
+  const planets = planetsObjects.map(planet => planet.name)
+
+  const field = document.querySelector('.field');
+  field.innerHTML = "";
+  const flex = document.createElement('div')
+  flex.className = 'flex'
+  flex.style.cssText =
+    `display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 15px;
+    max-width: 300px;`
+  field.appendChild(flex);
+
+    // divide planetsArray into arrays of 5 planets
+    let amountOnPage = 5;
+    let planetsSubArray = [];
+    for (let i = 0; i <Math.ceil(planets.length/amountOnPage); i++){
+      planetsSubArray[i] = planets.slice((i*amountOnPage), (i*amountOnPage) + amountOnPage);
+  }
+
+  function add5Planets(planetsArray) {
+
+  }
+
+  for (let i = 0; i < planets.length; i++) {
+    const planetCard = document.createElement("div");
+    planetCard.className = "planetCard";
+    planetCard.style.cssText = `
+    background-color: #cdeac0;
+    border: black solid 3px;
+    border-radius: 10px;
+    width: 500px;
+    height: 60px;
+    margin: 15px; 
+    display:flex;
+    justify-content:left;
+    padding-left: 10px;
+    align-items:center;      
+    `;
+    flex.appendChild(planetCard);
+
+    const name = document.createElement("p");
+    name.className = "name";
+    name.style.cssText =
+      "font-weight: 600; font-size: 22px; display: block; padding: 10px";
+    name.innerHTML = `${planets[i]}`;
+    planetCard.append(name);
+
+    const planetImg = document.createElement('img')
+    planetImg.className = 'planetImg'
+    planetImg.src = 'assets/globe.svg'
+    planetImg.style.cssText = 'display: block; padding: 10px 20px; width: 20px; height: 30px; margin-left: auto;'
+    planetCard.append(planetImg)
+    }
+
+    const nextButton = document.createElement("button");
+    nextButton.className = `styleButton`
+    nextButton.textContent = "Next";
+    flex.appendChild(nextButton);
+
+    nextButton.addEventListener('click', () => {
+      console.log("Next")
+    })
+
+  return planetsSubArray
+}
