fad81c5d807a9a8a2f6d9e7773b2915962b949dc
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-04-08 21:09:08 +0300

    add prod webpack config

---
+++ Notes/modules-in-browser/prod.webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ]
+};
