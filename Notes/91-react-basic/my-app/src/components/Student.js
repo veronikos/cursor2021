05ff23c6013b79dbd414749caeb59a4e0d908a93
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-04-09 11:11:42 +0300

    add own component

---
+++ Notes/91-react-basic/my-app/src/components/Student.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Student = () => {
+    return(
+        <div>
+            <h3>Name: </h3>
+            <p>Done exersises: </p>
+        </div>
+    )
+}
+
+export default Student;
--- Notes/91-react-basic/my-app/src/index.js
+++ Notes/91-react-basic/my-app/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// ReactDOM.render(
+//     React.createElement("div", {classname: "name"},
+//     React.createElement("a", {href: "#"}, "Link")),
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//     <div className="name">Hello Team</div>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
