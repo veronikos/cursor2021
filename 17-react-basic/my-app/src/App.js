60fd3ea4e7dc2861cecd7e974ef6d32a2e9bf1b0
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-04-11 11:29:38 +0300

    trying to deploy my project

--- 17-react-basic/my-app/src/App.js
+++ 17-react-basic/my-app/src/App.js
@@ -3,17 +3,21 @@ import UserInfo from './components/UserInfo'
 
 function Post(props) {
   return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-date">
-        {props.date}
+    <div className = "App-header">
+      <div className="Comment">
+        <UserInfo user={props.author} />
+          <div>
+              <div className="Comment-date">
+                {props.date}
+              </div>
+              <div className="Comment-text">{props.comment}</div>
+              <img
+              className="Image"
+              src={props.image}
+              alt={props.image}
+            />          
+          </div>
       </div>
-      <div className="Comment-text">{props.comment}</div>
-      <img
-      className="Image"
-      src={props.image}
-      alt={props.image}
-    />
     </div>
   );
 }
